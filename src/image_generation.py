6778444106b8f04eb85bc0d604414fedc7a807b0
nakata5321 <makar5321@gmail.com>
2021-10-13 23:00:27 +0300

    google trends

--- src/image_generation.py
+++ src/image_generation.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 from PIL import Image, ImageFont, ImageDraw
 from googletrans import Translator
+from std_msgs.msg import String
 import rospkg
 import rospy
+import pandas as pd
+from pytrends.request import TrendReq
 
 
 def create_image(data: str, dirpath: str) -> str:
@@ -18,7 +21,7 @@ def create_image(data: str, dirpath: str) -> str:
     img = Image.new("RGB", (1024, 1024), color=(255, 255, 255))
     draw = ImageDraw.Draw(img)
     # use a truetype font
-    font = ImageFont.truetype("../font/usual_font.ttc", 100)
+    font = ImageFont.truetype(dirpath + "/font/usual_font.ttc", 100)
     draw.text((512, 512), data, (0, 0, 0), font=font, anchor="mm")
     img.show()
     path_to_pic = dirpath + "result" + data + ".png"
@@ -27,22 +30,48 @@ def create_image(data: str, dirpath: str) -> str:
 
 
 def translate(word: str, language: str = 'ja') -> str:
+    """
+    translate word to Japanese language
+
+    :param word: given word to translate
+    :param language: destination language
+
+    :return:
+    """
+    rospy.loginfo(f"translate word to {language}")
     translator = Translator()
     trans = translator.translate(word, dest=language)
+    rospy.loginfo(f"translation is {trans.text}")
     return trans.text
+
+
+def get_word() -> str:
+    """
+    Request data from Google's Hot Searches section and return top word
+    """
+    pytrend = TrendReq()
+    df = pytrend.trending_searches()
+    return df[0][0]
 
 
 def main() -> None:
     # init ros node
     rospy.init_node("image_generation", anonymous=True, disable_signals=True)
+    pub = rospy.Publisher('run', String, queue_size=1)
     rospack = rospkg.RosPack()
     dirpath = rospack.get_path("image_generation")
-
+    rospy.loginfo("node is up")
+    # get word
+    rospy.loginfo("find the most popular word")
+    word = get_word()
+    rospy.loginfo(f"the word is: {word}")
     # translate word
-    tr_word = translate("Hello World!")
-
+    tr_word = translate(word)
     # create image
-    create_image(tr_word, dirpath)
+    rospy.loginfo("create picture with word")
+    path_to_pic = create_image(tr_word, dirpath)
+    rospy.loginfo(f"path to picture is: {path_to_pic}")
+    pub.publish(path_to_pic)
 
 
 if __name__ == "__main__":
